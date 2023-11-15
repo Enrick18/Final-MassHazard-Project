50f0d121b499e1b9316902c7b8449b9d03396e89
fertjuaquina@gmail.com <>
2023-11-15 20:10:29 +0800

    Revising AutoNavMesh

--- MazzHazardNew/Assets/AutoNavMesh.cs
+++ MazzHazardNew/Assets/AutoNavMesh.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class AutoNavMesh : MonoBehaviour
+public class AutoNavMeshBaker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NavMeshSurface navMeshSurface;
+
     void Start()
     {
-        
+        navMeshSurface.BuildNavMesh();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
